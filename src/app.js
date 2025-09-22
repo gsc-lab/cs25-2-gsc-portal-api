8807293906130a5cd73bc51410c5e7d291448756
Gapsick <gapsicke@gmail.com>
2025-09-22 20:13:42 +0900

    ✨ feat: 관리자 라우터 연결

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { swaggerUi, specs } from "./docs/swagger.js";
+
+//Router
+import adminRouter from './routes/adminRouter.js';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // Swagger 설정
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// api
+// 관리자
+app.use('/admin', adminRouter);
 
 
 export default app;
