c9395e5ac578a8ae962381e66db39f162c4c930f
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-22 21:06:04 +0900

    💅 style: 코드 포맷팅 및 구조 가독성 개선

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
-import globals from "globals";
 import js from "@eslint/js";
-import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
 
-export default [
-  // ESLint가 추천하는 기본 규칙 세트
-  js.configs.recommended,
-  // Prettier와 충돌하는 스타일 규칙을 비활성화
-  prettierConfig,
+export default defineConfig([
   {
-    languageOptions: {
-      globals: {
-        ...globals.node, // Node.js 전역 변수 인식
-      },
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    ignores: ["node_modules", "package-lock.json"]
   },
-];
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+    ignores: ["package-lock.json"]
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { swaggerUi, specs } from "./docs/swagger.js";
+
 import cookieParser from "cookie-parser";
 
-import authRouter from "./routes/auth.router.js";
 
+import authRouter from "./routes/authRouter.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 // Swagger 설정
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// api
+
+
+// 권한 & 인증
 app.use("/api/auth", authRouter);
-
-app.get("/", (req, res) => {
-  res.send(`<a href='/api/auth/'>구글</a>`);
-});
-
 export default app;
