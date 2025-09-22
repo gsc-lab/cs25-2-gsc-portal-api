d88fc3968cf3ab6dff9311656d85c759b7d3fb04
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-22 21:07:31 +0900

    ✨ feat: 인증/권한 라우터 추가

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,18 +1,14 @@
 import express from "express";
-import authController from "../controllers/auth/auth.controller.js";
-import refreshController from "../controllers/auth/refresh.controller.js";
+import authController from "../controllers/auth/authController.js";
+import refreshController from "../controllers/auth/refreshController.js";
 
 const router = express.Router();
 
-// 사용자를 Google OAuth 2.0 서버로 리디렉션하는 예시입니다.
+// 사용자를 Google OAuth 2.0 서버로 리디렉션
 router.get("/", authController.googleAuthRedirect);
 
-// Google OAuth 2.0 서버에서 콜백을 수신합니다.
+// Google OAuth 2.0 서버에서 콜백을 수신
 router.get("/callback", authController.authCallback);
-
-router.get("/register", (req, res) => {
-  res.render("register");
-});
 
 router.post("/register", authController.registerAfterOAuth);
 
