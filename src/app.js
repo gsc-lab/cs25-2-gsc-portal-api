4d06eeda4d980c72aace0cb75130356dfde9c3f6
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-04 10:53:45 +0900

    ♻️ refactor(app): application/xml 설명 주석 추가

--- src/app.js
+++ src/app.js
@@ -15,8 +15,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true })); // 클라이언트에서 application/x-www-form-urlencoded 데이터를 보냈을때 파싱해서 body 객체에 넣어줌
 // Swagger 설정
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 // 파일 디렉토리 설정
