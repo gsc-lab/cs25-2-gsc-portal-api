881c8a384832026a165e6bf1e2bdd99c523047fe
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-04 10:47:39 +0900

    ✨ feat(notice): 공지 발송·읽음처리·상태조회·과목필터 API 추가

--- src/controllers/noticeController.js
+++ src/controllers/noticeController.js
@@ -1,5 +1,5 @@
 import * as noticeService from "../service/notice-service.js";
-import {dispatchByNoticeId} from "../service/notice-service.js";
+import {dispatchByNoticeId, markNoticeAsRead} from "../service/notice-service.js";
 
 // 리스트
 export async function fetchNotices(req, res, next) {
@@ -32,6 +32,7 @@ export async function fetchNoticesId(req, res, next) {
 export async function createNotice(req, res, next) {
   try {
     const { user, body: noticeData, files } = req;
+    console.log('현재 로그인된사용자',user)
 
     const result = await noticeService.addNotice(user, noticeData, files);
 
@@ -76,9 +77,48 @@ export async function deleteNotice(req, res, next) {
 export async function dispatchNotice(req, res, next){
   try {
     const { notice_id } = req.params;
-    const { user } = req.user;
+    const { user } = req;
 
-    const result = await noticeService.dispatchByNoticeId(notice_id, user);
+    const jobInfo = await noticeService.dispatchByNoticeId(notice_id, user);
+
+    res.status(202).json(jobInfo);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function noticeAsRead(req, res, next) {
+  try {
+    const { notice_id } = req.params;
+    const { user } = req;
+
+    const result = await noticeService.markNoticeAsRead(notice_id, user);
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getNoticeStatus(req, res, next) {
+  try {
+    const { notice_id } = req.params;
+    const { user } = req;
+
+    const result = await noticeService.getNoticeReadStatusById(notice_id, user);
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getCourses(req, res, next) {
+  try {
+    const filters = req.query;
+    const { user } = req;
+
+    const result = await noticeService.filterCourses(user, filters);
 
     res.status(200).json(result);
   } catch (error) {
