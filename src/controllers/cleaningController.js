0b8243286808c55c92ceeffb29f0625a5e928855
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-08 00:00:52 +0900

    ✨ feat(cleaning): 청소 조회/생성/삭제 Controllers 추가

---
+++ src/controllers/cleaningController.js
@@ -0,0 +1,41 @@
+import * as cleaningService from "../service/cleaning_service.js";
+
+export const generateCleaningRosters = async (req, res, next) => {
+  try {
+    const rosterInfo = req.body;
+
+    const result = await cleaningService.generateRosters(rosterInfo);
+    res.status(201).json(result);
+  } catch (error) {
+    console.error("Error in generateCleaningRosters controller:", error);
+    next(error);
+  }
+};
+
+export const getCleaningRosters = async (req, res, next) => {
+  try {
+    const date = req.query.date || new Date();
+    let grade_id = req.query.grade_id;
+
+    const rosterData = await cleaningService.findRosterWeek(date, grade_id);
+    res.status(200).json(rosterData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteRosterByGrade = async (req, res, next) => {
+  try {
+    const { section, grade_id } = req.query;
+
+    if (!section) {
+      return res.status(400).json({ message: "학기는 필수 파라미터입니다." });
+    }
+
+    const result = await cleaningService.removeRosters(section, grade_id);
+
+    res.status(200).json({ result, message: "성곡적으로 삭제되었습니다." });
+  } catch (error) {
+    next(error);
+  }
+};
