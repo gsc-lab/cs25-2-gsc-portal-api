e920501129128b5161d35ed6b3792f5bad9b2113
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-24 23:40:28 +0900

    ♻️ refactor: AWS S3 Multer 연동 코드 업데이트

--- src/middleware/fileMiddleware.js
+++ src/middleware/fileMiddleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
+import AWS from "aws-sdk";
+import multerS3 from "multer-s3";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
-// 공통 스토리지
+// 공통 스토리지 -> upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -16,6 +18,27 @@ const storage = multer.diskStorage({
   },
 });
 
+// AWS region 및 자격증명 설정
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2'
+})
+
+// AWS S3 multer 설정
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'gsc-portal-files',
+    acl: "uploads",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key(req, file, cb) {
+      cb(null, `${Date.now()}_${path.basename(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+})
+
 // 허용되는 파일 타입과 사이즈
 const createUploadMiddleware = (fileSizeLimitMB) => {
   return multer({
@@ -25,6 +48,9 @@ const createUploadMiddleware = (fileSizeLimitMB) => {
     },
   });
 };
+
+// 로컬 multer 설정
+const upload2 = multer();
 
 // 공지사항
 export const uploadForNotices = createUploadMiddleware(50);
