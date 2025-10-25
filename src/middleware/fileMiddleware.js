66aa301f639e86884c3dab2d19780c6be149f4cf
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-25 16:32:38 +0900

    ♻️ refactor: AWS S3 주석처리

--- src/middleware/fileMiddleware.js
+++ src/middleware/fileMiddleware.js
@@ -18,36 +18,36 @@ const storage = multer.diskStorage({
   },
 });
 
-// AWS region 및 자격증명 설정
-AWS.config.update({
-  accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: 'ap-northeast-2'
-})
-
-// AWS S3 multer 설정
-const upload = multer({
-  storage: multerS3({
-    s3: new AWS.S3(),
-    bucket: 'gsc-portal-files',
-    acl: "uploads",
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key(req, file, cb) {
-      cb(null, `${Date.now()}_${path.basename(file.originalname)}`);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-})
-
-// 허용되는 파일 타입과 사이즈
-const createUploadMiddleware = (fileSizeLimitMB) => {
-  return multer({
-    storage,
-    limits: {
-      fileSize: fileSizeLimitMB * 1024 * 1024,
-    },
-  });
-};
+// // AWS region 및 자격증명 설정
+// AWS.config.update({
+//   accessKeyId: process.env.S3_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+//   region: 'ap-northeast-2'
+// })
+//
+// // AWS S3 multer 설정
+// const upload = multer({
+//   storage: multerS3({
+//     s3: new AWS.S3(),
+//     bucket: 'gsc-portal-files',
+//     acl: "uploads",
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     key(req, file, cb) {
+//       cb(null, `${Date.now()}_${path.basename(file.originalname)}`);
+//     },
+//   }),
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// })
+//
+// // 허용되는 파일 타입과 사이즈
+// const createUploadMiddleware = (fileSizeLimitMB) => {
+//   return multer({
+//     storage,
+//     limits: {
+//       fileSize: fileSizeLimitMB * 1024 * 1024,
+//     },
+//   });
+// };
 
 // 로컬 multer 설정
 const upload2 = multer();
